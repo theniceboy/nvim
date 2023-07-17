bb73825dbc9e9a0e6fde053c3f993c9271344858
David Chen <darkwei233@gmail.com>
2023-07-17 11:13:04 +0800

    nvim notify style customizations

--- lua/config/plugins/notify.lua
+++ lua/config/plugins/notify.lua
@@ -4,15 +4,31 @@ return {
 		local notify = require("notify")
 		vim.notify = notify
 		notify.setup({
-			background_colour = "NotifyBackground",
+			on_open = function(win)
+				vim.api.nvim_win_set_config(win, { border = "none" })
+			end,
+			background_colour = "#202020",
 			fps = 30,
 			level = 2,
 			minimum_width = 50,
 			render = "compact",
-			stages = "fade",
-			timeout = 5000,
+			stages = "fade_in_slide_out",
+			timeout = 3000,
 			top_down = true
-
 		})
+		local opts = { noremap = true, silent = true }
+		vim.keymap.set("n", ",;", function()
+			require('telescope').extensions.notify.notify({
+				layout_strategy = 'vertical',
+				layout_config = {
+					width = 0.9,
+					height = 0.9,
+					-- preview_height = 0.1,
+				},
+				wrap_results = true,
+				previewer = false,
+			})
+		end, opts);
+		vim.keymap.set("n", "<LEADER>c;", notify.dismiss, opts);
 	end
 }
