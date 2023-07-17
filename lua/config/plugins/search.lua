b0da428340726c4863f8d23dcc345a37f242e54e
David Chen <darkwei233@gmail.com>
2023-07-17 11:13:48 +0800

    added nvim-specture

--- lua/config/plugins/search.lua
+++ lua/config/plugins/search.lua
@@ -26,5 +26,21 @@ return {
 			vim.g.any_jump_window_width_ratio = 0.9
 			vim.g.any_jump_window_height_ratio = 0.9
 		end
+	},
+	{
+		"nvim-pack/nvim-spectre",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		keys = {
+			{
+				"<leader>F",
+				mode = "n",
+				function()
+					require("spectre").open()
+				end,
+				desc = "Project find and replace"
+			}
+		}
 	}
 }
