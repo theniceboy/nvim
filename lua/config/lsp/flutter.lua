2163e7536b9cf130a03310e65918a986f4f8838f
David Chen <darkwei233@gmail.com>
2023-07-27 22:52:24 +0800

    misc

--- lua/config/lsp/flutter.lua
+++ lua/config/lsp/flutter.lua
@@ -1,37 +1,32 @@
+local preview_stack_trace = function()
+	local line = vim.api.nvim_get_current_line()
+	local pattern = "package:[^/]+/([^:]+):(%d+):(%d+)"
+	local filepath, line_nr, column_nr = string.match(line, pattern)
+	if filepath and line_nr and column_nr then
+		vim.cmd(":wincmd k")
+		vim.cmd("e " .. filepath)
+		vim.api.nvim_win_set_cursor(0, { tonumber(line_nr), tonumber(column_nr) })
+		vim.cmd(":wincmd j")
+	end
+end
 return {
-	preview_stack_trace = function()
-		local line = vim.api.nvim_get_current_line()
-		local pattern = '%(package:[^/]+/([^:]+):(%d+):(%d+)%)'
-		local filepath, line_nr, col_nr = line:match(pattern)
-		if filepath and line_nr and col_nr then
-			vim.cmd(":wincmd k")
-			vim.cmd('e ' .. filepath)
-			vim.api.nvim_win_set_cursor(0, { tonumber(line_nr), tonumber(col_nr) })
-			vim.cmd(":wincmd j")
-		end
-	end,
 	setup = function(lsp)
-		vim.cmd([[
-		  augroup flutter_log_keybinds_autocmd_group
-			  autocmd!
-			  autocmd BufEnter __FLUTTER_DEV_LOG__ nnoremap <buffer> p :lua require("config.lsp.flutter").preview_stack_trace()<CR>
-			augroup END
-		]])
-
+		vim.api.nvim_create_autocmd("BufEnter", {
+			pattern = "__FLUTTER_DEV_LOG__",
+			callback = function()
+				vim.keymap.set("n", "p", preview_stack_trace, { silent = true, noremap = true, buffer = true })
+			end
+		})
 		local dart_lsp = lsp.build_options('dartls', {})
 
-		require('flutter-tools').setup({
+		local flutter = require('flutter-tools')
+		flutter.setup({
 			fvm = true,
 			widget_guides = {
 				enabled = true,
 			},
 			ui = {
-				-- the border type to use for all floating windows, the same options/formats
-				-- used for ":h nvim_open_win" e.g. "single" | "shadow" | {<table-of-eight-chars>}
 				border = "rounded",
-				-- This determines whether notifications are show with `vim.notify` or with the plugin's custom UI
-				-- please note that this option is eventually going to be deprecated and users will need to
-				-- depend on plugins like `nvim-notify` instead.
 				notification_style = 'nvim-notify'
 			},
 			lsp = {
@@ -66,7 +61,7 @@ return {
 						filter = 'raised',
 						label = 'Exceptions',
 						condition =
-						"!(url:startsWith('package:flutteR/') || url:startsWith('package:flutter_test/') || url:startsWith('package:dartpad_sample/') || url:startsWith('package:flutter_localizations/'))"
+						"!(url:startsWith('package:flutter/') || url:startsWith('package:flutter_test/') || url:startsWith('package:dartpad_sample/') || url:startsWith('package:flutter_localizations/'))"
 					}
 				},
 				register_configurations = function(_)
--- lua/plugin/compile_run.lua
+++ lua/plugin/compile_run.lua
@@ -1,3 +1,8 @@
+local split = function()
+	vim.cmd("set splitbelow")
+	vim.cmd("sp")
+	vim.cmd("res -5")
+end
 local compileRun = function()
 	vim.cmd("w")
 	-- check file type
@@ -6,6 +11,9 @@ local compileRun = function()
 		vim.cmd(":FlutterRun -d " .. vim.g.flutter_default_device .. " " .. vim.g.flutter_run_args .. "<CR>")
 	elseif ft == "markdown" then
 		vim.cmd(":InstantMarkdownPreview<CR>")
+	elseif ft == 'lua' then
+		split()
+		vim.cmd("term luajit %")
 	end
 end
 
