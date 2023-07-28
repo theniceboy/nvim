84f7bf360e970a85ccfea8b726688adc19fe6bb3
David Chen <darkwei233@gmail.com>
2023-07-28 23:22:13 +0800

    added js treesitter

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -7,7 +7,20 @@ return {
 		build = ":TSUpdate",
 		config = function()
 			require("nvim-treesitter.configs").setup({
-				ensure_installed = { "query", "typescript", "dart", "java", "c", "prisma", "bash", "go", "lua", "html", "vim" },
+				ensure_installed = {
+					"html",
+					"javascript",
+					"typescript",
+					"query",
+					"dart",
+					"java",
+					"c",
+					"prisma",
+					"bash",
+					"go",
+					"lua",
+					"vim"
+				},
 				highlight = {
 					enable = true,
 					disable = {}, -- list of language that will be disabled
