1781e782d6d705c732bffe4e26eaba39bbf2a9a2
David Chen <darkwei233@gmail.com>
2023-10-09 12:50:24 -0700

    Comment.nvim -> tcomment_vim

--- lua/config/plugins/comment.lua
+++ lua/config/plugins/comment.lua
@@ -1,29 +1,16 @@
 return {
-	"numToStr/Comment.nvim",
+	"tomtom/tcomment_vim",
+	event = "BufRead",
 	config = function()
-		local api = require("Comment.api")
-		vim.keymap.set("n", "<leader>cn", api.locked("comment.linewise.current"))
-		vim.keymap.set("n", "<leader>cu", api.locked("uncomment.linewise.current"))
-		local esc = vim.api.nvim_replace_termcodes('<ESC>', true, false, true)
-		vim.keymap.set('x', '<leader>cn', function()
-			vim.api.nvim_feedkeys(esc, 'nx', false)
-			api.locked('comment.linewise')(vim.fn.visualmode())
-		end)
-		vim.keymap.set('x', '<leader>cu', function()
-			vim.api.nvim_feedkeys(esc, 'nx', false)
-			api.locked('uncomment.linewise')(vim.fn.visualmode())
-		end)
-		vim.keymap.set('x', '<leader>ce', function()
-			vim.api.nvim_feedkeys(esc, 'nx', false)
-			api.locked('toggle.blockwise')(vim.fn.visualmode())
-		end)
-		require('Comment').setup({
-			padding = true,
-			sticky = true,
-			mappings = {
-				basic = false,
-				extra = false,
-			},
-		})
+		vim.g.tcomment_maps = true
+		vim.g.tcomment_textobject_inlinecomment = ''
+
+		vim.cmd([[
+nmap <LEADER>cn g>c
+vmap <LEADER>cn g>
+nmap <LEADER>cu g<c
+vmap <LEADER>cu g<
+unmap ic
+		]])
 	end
 }
