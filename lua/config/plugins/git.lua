1b874d1139173cd2e96fa6ab17c5ffb4a1dd7c52
David Chen <darkwei233@gmail.com>
2024-07-26 10:36:41 -0500

    removed deprecation warning

--- lua/config/plugins/git.lua
+++ lua/config/plugins/git.lua
@@ -4,12 +4,12 @@ return {
 		config = function()
 			require('gitsigns').setup({
 				signs = {
-					add          = { hl = 'GitSignsAdd', text = '▎', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
-					change       = { hl = 'GitSignsChange', text = '░', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
-					delete       = { hl = 'GitSignsDelete', text = '_', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-					topdelete    = { hl = 'GitSignsDelete', text = '▔', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-					changedelete = { hl = 'GitSignsChange', text = '▒', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
-					untracked    = { hl = 'GitSignsAdd', text = '┆', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
+					add          = { text = '▎' },
+					change       = { text = '░' },
+					delete       = { text = '_' },
+					topdelete    = { text = '▔' },
+					changedelete = { text = '▒' },
+					untracked    = { text = '┆' },
 				},
 			})
 			vim.keymap.set("n", "<leader>g-", ":Gitsigns prev_hunk<CR>", { noremap = true, silent = true })
