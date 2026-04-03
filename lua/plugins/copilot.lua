35e4e084d5240ce686a097c2ef3fdcfb173a37dc
David Chen <darkwei233@gmail.com>
2026-04-02 22:01:43 -0700

    disable copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -108,25 +108,25 @@ return {
 	-- 		})
 	-- 	end,
 	-- },
-	{
-		"github/copilot.vim",
-		config = function()
-			vim.g.copilot_enabled = true
-			vim.g.copilot_no_tab_map = true
-			vim.api.nvim_set_keymap('n', '<leader>go', ':Copilot<CR>', { silent = true })
-			vim.api.nvim_set_keymap('n', '<leader>ge', ':Copilot enable<CR>', { silent = true })
-			vim.api.nvim_set_keymap('n', '<leader>gd', ':Copilot disable<CR>', { silent = true })
-			vim.api.nvim_set_keymap('i', '<c-p>', '<Plug>(copilot-suggest)', { noremap = true })
-			vim.api.nvim_set_keymap('i', '<c-n>', '<Plug>(copilot-next)', { noremap = true, silent = true })
-			vim.api.nvim_set_keymap('i', '<c-l>', '<Plug>(copilot-previous)', { noremap = true, silent = true })
-			vim.cmd('imap <silent><script><expr> <C-C> copilot#Accept("")')
-			vim.cmd([[
-			let g:copilot_filetypes = {
-	       \ 'TelescopePrompt': v:false,
-	     \ }
-			]])
-		end
-	}
+	-- {
+	-- 	"github/copilot.vim",
+	-- 	config = function()
+	-- 		vim.g.copilot_enabled = true
+	-- 		vim.g.copilot_no_tab_map = true
+	-- 		vim.api.nvim_set_keymap('n', '<leader>go', ':Copilot<CR>', { silent = true })
+	-- 		vim.api.nvim_set_keymap('n', '<leader>ge', ':Copilot enable<CR>', { silent = true })
+	-- 		vim.api.nvim_set_keymap('n', '<leader>gd', ':Copilot disable<CR>', { silent = true })
+	-- 		vim.api.nvim_set_keymap('i', '<c-p>', '<Plug>(copilot-suggest)', { noremap = true })
+	-- 		vim.api.nvim_set_keymap('i', '<c-n>', '<Plug>(copilot-next)', { noremap = true, silent = true })
+	-- 		vim.api.nvim_set_keymap('i', '<c-l>', '<Plug>(copilot-previous)', { noremap = true, silent = true })
+	-- 		vim.cmd('imap <silent><script><expr> <C-C> copilot#Accept("")')
+	-- 		vim.cmd([[
+	-- 		let g:copilot_filetypes = {
+	--        \ 'TelescopePrompt': v:false,
+	--      \ }
+	-- 		]])
+	-- 	end
+	-- }
 	-- {
 	-- 	"Exafunction/codeium.vim",
 	-- 	config = function()
